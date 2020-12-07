833a6cb719b81b92d957ff7ccb67e73b3ce97c73
Brenda Goodwin <brendanced@gmail.com>
2020-12-06 21:47:22 -0700

    user can add dept and roles

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var consoleTable = require("console.table");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var queries = require("./queries");
+var setUp = require("./setup");
+const { resolve } = require("url");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -19,67 +21,196 @@ connection.connect(function (err) {
 
 function firstStart() {
   console.log(
-    "Welcome to easyEmp! \n A CMS to help you manage your employee database \n Type in a type of employee\n (i.e. vice president, progammer, salesperson, etc.)\n You will have a chance to enter all employee types"
+    "Welcome to easyEmp! \n A CMS to help you manage your employee database"
   );
-  empRole();
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "setUp",
+        message:
+          "Do you need to set up or update either your employee types or department names?",
+      },
+    ])
+    .then(function (answer) {
+      if (answer.setUp === true) {
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "setUpmodify",
+              message: "Which would you like to add?",
+              choices: ["Employee Type", "Department Name", "Both"],
+            },
+          ])
+          .then(function (answer) {
+            switch (answer.setUpmodify) {
+              case "Employee Type":
+                console.log(
+                  "Type in a type of employee\n (i.e. vice president, progammer, salesperson, etc.)\n one at a time"
+                );
+                empRole();
+                break;
+
+              case "Department Name":
+                console.log(
+                  "Enter Departments (i.e. accounting, production, sales etc.) \n one at a time"
+                );
+                deptName();
+            }
+          });
+      } else {
+        start();
+      }
+    });
 
   function empRole() {
     inquirer
-      .prompt(
+      .prompt([
         {
           type: "input",
           name: "empType",
           message: "Employee Type",
         },
         {
-          type: "confirm",
-          name: "done",
-          message: "Do you have more employee types to enter?",
-        }
-      )
+          type: "input",
+          name: "salary",
+          message: "Salary (in K, i.e 40,500 = 40.5)",
+        },
+        {
+          type: "input",
+          name: "dept",
+          message: "Department (if Applicable, type enter for none)",
+        },
+      ])
       .then(function (answer) {
-        if (answer.done === false) {
-          empRole();
-        } else {
-          console.log(
-            "Thank you for emtering your employee types. Now you may perform the followin tasks:"
-          );
-          start();
+        connection.query(
+          "INSERT INTO role SET ?",
+          {
+            title: answer.empType,
+            salary: answer.salary,
+            department_id: answer.dept,
+          },
+          function (err) {
+            if (err) throw err;
+            console.log(`Employee type ${answer.empType} edded to database`);
+            inquirer
+              .prompt([
+                {
+                  type: "confirm",
+                  name: "done",
+                  message: "Would you like to add another employee type?",
+                },
+              ])
+              .then(function (answer) {
+                if (answer.done === false) {
+                  console.log("Thank you for emtering your employee types.");
+                  start();
+                } else {
+                  empRole();
+                }
+              });
+          }
+        );
+      });
+  }
+
+  function deptName() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "deptName",
+          message: "Department Name:",
+        },
+        {
+          type: "list",
+          name: "done",
+          message: "Do you have more Department Names to enter?",
+          choices: ["yes", "no"],
+        },
+      ])
+      .then(function (answer) {
+        connection.query(
+          "INSERT INTO department SET ?",
+          {
+            name: answer.deptName,
+          },
+          function (err) {
+            if (err) throw err;
+            console.log(`${answer.deptName} Department added to database`);
+            inquirer
+              .prompt([
+                {
+                  type: "confirm",
+                  name: "done",
+                  message: "Would you like to add another department name?",
+                },
+              ])
+              .then(function (answer) {
+                if (answer.done === false) {
+                  console.log(
+                    "Thank you for emtering your department names. Now you may perform the following tasks:"
+                  );
+                  start();
+                } else {
+                  deptName();
+                }
+              });
+          }
+        );
+      });
+  }
+  function displayRoleNo() {
+    console.log("Employee-type Id#s");
+    connection.query("SELECT id, title FROM role"),
+      function (err, res) {
+        if (err) throw err;
+        console.log(res);
+      };
+  }
+  function displayDeptNo() {
+    console.log("Department Id#s");
+    connection.query("SELECT id, name FROM department"),
+      function (err, res) {
+        if (err) throw err;
+        console.log(res);
+      };
+  }
+
+  function start() {
+    displayRoleNo();
+    displayDeptNo();
+    inquirer
+      .prompt({
+        type: "list",
+        name: "taskChoice",
+        message: "Choose your task",
+        choices: [
+          "Add new info to the database",
+          "View database info",
+          "Change current info in database",
+          "Remove database information or entries",
+        ],
+      })
+      .then(function (answer) {
+        switch (answer) {
+          case "Add new info to the database":
+            addInfo();
+            break;
+
+          case "View database info":
+            viewInfo();
+            break;
+
+          case "Change current info in database":
+            changeInfo();
+            break;
+
+          case "Remove database information or entries":
+            removeInfo();
+            break;
         }
       });
   }
-}
-
-function start() {
-  inquirer
-    .prompt({
-      type: "list",
-      name: "taskChoice",
-      message: "Choose your task",
-      choices: [
-        "Add new info to the database",
-        "View database info",
-        "Change current info in database",
-        "Remove database information or entries",
-      ],
-    })
-    .then(function (answer) {
-      switch (answer) {
-        case "Add new info to the database":
-          addInfo();
-          break;
-
-        case "View database info":
-          viewInfo();
-          break;
-
-        case "Change current info in database":
-          changeInfo();
-          break;
-
-        case "Remove database information or entries":
-          removeInfo();
-          break;
-      }
-    });
 }
